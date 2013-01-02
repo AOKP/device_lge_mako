384caa0ac40e3306d845ac9601629730345ce6c2
Roman Birg <romanbirg@gmail.com>
2013-01-02 04:56:05 -0600

    mako: update stock kernel with EHCI fixes (thank you faux123 for the kernel!)
    Change-Id: I4aa4f8ca93cc43e3e5d0b0b66c24fda7603a1f11
    Signed-off-by: Roman Birg <romanbirg@gmail.com>

--- device.mk
+++ device.mk
@@ -42,7 +42,7 @@ PRODUCT_PACKAGES += \
         VisualizationWallpapers \
         librs_jni
 
-LOCAL_KERNEL := device/lge/mako-kernel/kernel
+LOCAL_KERNEL := device/lge/mako/kernel
 
 PRODUCT_COPY_FILES := \
 	$(LOCAL_KERNEL):kernel
