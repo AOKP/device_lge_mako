294c13ee858e3365ec971babe341aa9cb93ae7e8
STELIX <ssspinni@gmail.com>
2016-01-21 11:21:46 +0100

    mako - AOKPize [1/2]
    Change-Id: I71363e6bd8aa713185980cbef810494ad6146d13
    Signed-off-by: STELIX <ssspinni@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -3,17 +3,17 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 768
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/mako/full_mako.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mako
-PRODUCT_NAME := cm_mako
+PRODUCT_NAME := aokp_mako
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 4
 PRODUCT_MANUFACTURER := LGE
